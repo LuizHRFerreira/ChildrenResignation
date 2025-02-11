4cd2c8f186755ce668ea7675c5ce260d5e1ec7e3
luizagt <luiz.augusto9922@outlook.com>
2025-02-11 05:45:12 -0300

    feat: ajusts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,45 +38,7 @@
         @inertiaHead
     </head>
     <body class="layout-fixed sidebar-expand-lg sidebar-mini bg-body-tertiary" data-bs-theme="dark">
-        <div class="app-wrapper">
-            <!--begin::Header-->
-            @include('layouts.header')
-            <!--end::Header-->
-
-            <!--begin::Sidebar-->
-            @include('layouts.sidebar')
-            <!--end::Sidebar-->
-
-            <!--begin::App Main-->
-            <main class="app-main">
-                <!--begin::App Content Header-->
-                <div class="app-content-header">
-                <!--begin::Container-->
-                <div class="container-fluid">
-                    @inertia
-                </div>
-                <!--end::Container-->
-                </div>
-                <!--end::App Content Header-->
-            </main>
-            <!--end::App Main-->
-
-            <!--begin::Footer-->
-            <footer class="app-footer">
-                <!--begin::To the end-->
-                <div class="float-end d-none d-sm-inline">Anything you want</div>
-                <!--end::To the end-->
-                <!--begin::Copyright-->
-                <strong>
-                Copyright &copy; 2014-2024&nbsp;
-                <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
-                </strong>
-                All rights reserved.
-                <!--end::Copyright-->
-            </footer>
-            <!--end::Footer-->
-        </div>
-            <!--end::App Wrapper-->
+        @inertia
         <!--begin::Script-->
         <!--begin::Third Party Plugin(OverlayScrollbars)-->
         <script src="{{ asset('js/overlayscrollbars.browser.es6.min.js') }}"></script>
